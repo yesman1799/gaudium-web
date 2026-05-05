c01093049816733aacd01ba40932867d8fd51bbb
yesman1799 <smradoch12@gmail.com>
2026-05-05 14:36:18 +0200

    eshop button added

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src') // Allows @ to refer to src/
-    }
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
+  base: './',            // <<< DŮLEŽITÉ PRO FTP / SHARED HOSTING
   build: {
-    target: 'esnext' // This enables top-level await support
-  }
-});
+    target: 'esnext',
+  },
+})
